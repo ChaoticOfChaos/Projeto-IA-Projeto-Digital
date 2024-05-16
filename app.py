7e5f5d5ba438d98ac691c36921d473d2ea38a45b
ChaoticOfChaos <143908924+ChaoticOfChaos@users.noreply.github.com>
2024-05-16 17:02:21 -0300

    Atualização do app.py
    Agora foi criado um sistema que mostra ao usuário quais campos não foram preenchidos

--- app.py
+++ app.py
@@ -21,12 +21,42 @@ def processar():
     email = request.form['email']
     tipo = request.form['tipo']
 
-    # Tratamento dos Dados Recebidos
+    # Variável utilizada para saber qual e se algum campo não foi preenchido
+    noneInputs = []
+
+    for c, i in enumerate([nome, email, tipo]):
+        # Verifica Se os campos NÃO foram preenchidos
+        if not i:
+            noneInputs.append(c)
+            i = None
+
+    # Verifica quais campos não foram preenchidos
+    noneInputsRefinada = []
+    for i in noneInputs:
+        match i:
+            case 0:
+                noneInputsRefinada.append('nome')
+            case 1:
+                noneInputsRefinada.append('email')
+            case 2:
+                noneInputsRefinada.append('tipo')
+
+    # Imprime os Campos Preenchidos
     print(f'Nome: {nome}')
     print(f'Email: {email}')
     print(f'Tipo da Empresa: {tipo}')
 
-    return "Dados Recebidos Com Sucesso!"
+    # Caso todos os campos sejam preenchidos
+    if len(noneInputsRefinada) == 0:
+        return "Dados Recebidos Com Sucesso!"
+    
+    # Caso algum campo não seja preenchido
+    errorString = 'Erro! Campos Não Preenchidos: '
+    for i in noneInputsRefinada:
+        errorString += i + ' '
 
+    return errorString
+
+# Inicializa a Aplicação
 if __name__ == '__main__':
     app.run(debug=True)
